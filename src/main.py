53548a0dd996345c38e838779098ce4796119102
Gorm Reventlow <gorm@reventlow.com>
2023-04-02 08:25:59 +0200

    inital

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 import os
-import docx2pdf
 import time
+import subprocess
 
 # set directory path
-directory = "/docx2pdf"
+directory = "/path/to/docx/files"
+
+# initialize last conversion time as current time
+last_conversion_time = time.time()
 
 while True:
     # loop through each file in directory
@@ -17,9 +20,18 @@ while True:
             if os.path.isfile(output_path):
                 print(f"{output_path} already exists. Skipping conversion.")
             else:
-                # convert docx to pdf
-                docx2pdf.convert(input_path, output_path)
-                print(f"{input_path} converted to {output_path}")
+                # check if file has been modified since last conversion
+                if os.path.getmtime(input_path) > last_conversion_time:
+                    # create PDF converter command
+                    cmd = ["libreoffice", "--headless", "--convert-to", "pdf", "--outdir", directory, input_path]
+
+                    # convert docx to pdf using libreoffice
+                    subprocess.run(cmd)
+
+                    print(f"{input_path} converted to {output_path}")
+
+                    # update last conversion time to current time
+                    last_conversion_time = time.time()
 
     # wait for 10 seconds before looping again
     time.sleep(10)
